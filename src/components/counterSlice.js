c4ec8307009b39df813e800a9e106696db344ebf
yamboni1 <ncyambot.work@gmail.com>
2023-08-29 15:07:26 +0800

    "feat: added initial implementation of add"

--- src/components/counterSlice.js
+++ src/components/counterSlice.js
@@ -1,18 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+
 
 const counterSlice = createSlice({
     name: "counter",
     initialState: {
         counterList: [],
+        sum: 0
     },
     reducers: {
         updateSize: (state, action) => {
             state.counterList = new Array(action.payload).fill(0);
             console.log(action);
         },
-        updateCount: (state, count) => {},
+        updateCount: (state, action) => {
+            console.log(action.payload);
+        },
     },
 });
 
-export const { updateSize } = counterSlice.actions;
+export const { updateSize,updateCount } = counterSlice.actions;
 export default counterSlice.reducer;
